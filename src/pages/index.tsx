10b1fc653a6806610039a17c35d27f72e7b77d19
Fabio Duarte <fabio.ivi@gmail.com>
2022-04-19 16:28:11 -0400

    feat: organize project folders

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>STAEM</title>
+        <meta name="description" content="Games" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Teste</h1>
+    </div>
+  )
+}
+
+export default Home
