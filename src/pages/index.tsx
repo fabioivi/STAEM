9606b3b57030b0e472139472a62b1cf63ed2ef6d
Fabio Duarte <fabio.ivi@gmail.com>
2022-04-19 20:42:35 -0400

    feat: add axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { CardList } from '../components/CardList'
 import { Header } from '../components/Header'
 
 const Home: NextPage = () => {
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+      <CardList/>
     </div>
   )
 }
