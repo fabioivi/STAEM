0900c9b4ae27205967cc952c16958ab7b7f474e5
Fabio Duarte <fabio.ivi@gmail.com>
2022-04-19 20:38:18 -0400

    feat: add color to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { Flex, Spacer, Button, Heading } from "@chakra-ui/react"
 export function Header(){
     return (
-        <Flex p="4">
+        <Flex p="4" bg="brand.darkBlue" color="brand.white">
             <Heading>STAEM</Heading>
             <Spacer />
             <Button>Install</Button>
--- styles/themes.js
+++ styles/themes.js
@@ -3,14 +3,15 @@ import { extendTheme } from '@chakra-ui/react'
 // 2. Extend the theme to include custom colors, fonts, etc
 const colors = {
   brand: {
-    900: '#1a365d',
+    darkBlue: '#1a365d',
+    white: "#FFFFFF",
     800: '#153e75',
     700: '#2a69ac',
   },
   fonts: {
     heading: "Noto Sans",
     body: "Arial",
-  }
+  },
 }
 
 export default extendTheme({ colors })
