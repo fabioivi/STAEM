98a0cf8774ef633b427626cc533e82362971b5f6
Fabio Duarte <fabio.ivi@gmail.com>
2022-04-25 18:55:37 -0400

    feat: add icon in button install

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,23 @@
-import { Flex, Spacer, Button, Heading } from "@chakra-ui/react"
+import { Flex, Spacer, Button, Heading, Image} from "@chakra-ui/react"
 
 export function Header(){
     return (
-        <Flex p="4" bg="brand.darkblue" color="brand.white">
-            <Heading>STAEM</Heading>
+        <Flex pl="52.3px" pr="52.3px"  h="132px" align="center" bg="brand.darkblue" color="brand.white">
+            <Heading  fontWeight="Bold" fontSize="40px" lineHeight="38.24px">STAEM</Heading>
             <Spacer />
-            <Button>Install</Button>
+            <Button
+                leftIcon={<Image src="/assets/Install.svg" alt="install icon"  />}
+                bg="#214B6B" 
+                w="153px" 
+                h="40px" 
+                borderRadius="30px" 
+                fontFamily="Noto Sans" 
+                fontSize="16px" 
+                lineHeight="15.3px"
+                fontWeight="Bold" 
+            >
+            Install
+            </Button>
         </Flex>
     )
 }
