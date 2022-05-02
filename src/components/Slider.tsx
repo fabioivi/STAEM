47d20e7c535da36117e8bddc347c78d591b07d41
Fábio Duarte de Oliveira <fabio.ivi@gmail.com>
2022-05-02 19:00:57 -0400

    feat: customize css of slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,148 +1,36 @@
-import { Box, Heading, Image, Center} from "@chakra-ui/react";
+import { Box, Heading, Image, Center } from "@chakra-ui/react";
 import { useState } from "react";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 import { EffectFade, Navigation, Pagination } from "swiper";
 
-export function Slider(){
-    return (
-       
-        <Swiper
-            slidesPerView={3}
-            spaceBetween={70}
-            navigation
-            pagination
-            className="mySwiper"
-            >
-                <SwiperSlide>
-                    <Center
-                        position="relative"
-                        flexDir="column"
-                        w="100%"
-                        h="100%"
-                        //overflow="hidden"
-                        bg="white"
-                    >
-                        <Image
-                            width="100%"
-                            src=""
-                            alt="slider image"
-                            bgSize="cover"
-                            bgAttachment="fixed"
-                            bgRepeat="no-repeat"
-                            src="https://cdn.akamai.steamstatic.com/steam/apps/1940190/header.jpg?t=1649487691"
-                        /> 
-                    </Center>   
-                </SwiperSlide>
+export function Slider() {
+  return (
+    <Swiper
+      slidesPerView={2}
+      spaceBetween={150}
+      centeredSlides={true}
+      grabCursor={true}
+      loop={true}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <Image src="https://cdn.akamai.steamstatic.com/steam/apps/1245620/header.jpg?t=1649774637" />
+      </SwiperSlide>
 
-                <SwiperSlide>
-                    <Center
-                        position="relative"
-                        flexDir="column"
-                        w="100%"
-                        h="100%"
-                        overflow="hidden"
-                        bg="white"
-                    >
-                        <Image
-                            width="1000px"
-                            src="https://cdn.akamai.steamstatic.com/steam/apps/1021100/header.jpg?t=1650647590"
-                            alt="slider image"
-                            bgSize="cover"
-                            bgAttachment="fixed"
-                            bgRepeat="no-repeat"
-                        /> 
-                    </Center>   
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Center
-                        position="relative"
-                        flexDir="column"
-                        w="100%"
-                        h="100%"
-                        //overflow="hidden"
-                        bg="white"
-                    >
-                        <Image
-                            width="100%"
-                            src=""
-                            alt="slider image"
-                            bgSize="cover"
-                            bgAttachment="fixed"
-                            bgRepeat="no-repeat"
-                            src="https://cdn.akamai.steamstatic.com/steam/apps/1940190/header.jpg?t=1649487691"
-                        /> 
-                    </Center>   
-                </SwiperSlide>
-                
-                <SwiperSlide>
-                    <Center
-                        position="relative"
-                        flexDir="column"
-                        w="100%"
-                        h="100%"
-                        //overflow="hidden"
-                        bg="white"
-                    >
-                        <Image
-                            width="100%"
-                            src=""
-                            alt="slider image"
-                            bgSize="cover"
-                            bgAttachment="fixed"
-                            bgRepeat="no-repeat"
-                            src="https://cdn.akamai.steamstatic.com/steam/apps/1940190/header.jpg?t=1649487691"
-                        /> 
-                    </Center>   
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Center
-                        position="relative"
-                        flexDir="column"
-                        w="100%"
-                        h="100%"
-                        //overflow="hidden"
-                        bg="white"
-                    >
-                        <Image
-                            width="100%"
-                            src=""
-                            alt="slider image"
-                            bgSize="cover"
-                            bgAttachment="fixed"
-                            bgRepeat="no-repeat"
-                            src="https://cdn.akamai.steamstatic.com/steam/apps/1940190/header.jpg?t=1649487691"
-                        /> 
-                    </Center>   
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Center
-                        position="relative"
-                        flexDir="column"
-                        w="100%"
-                        h="100%"
-                        //overflow="hidden"
-                        bg="white"
-                    >
-                        <Image
-                            width="100%"
-                            src=""
-                            alt="slider image"
-                            bgSize="cover"
-                            bgAttachment="fixed"
-                            bgRepeat="no-repeat"
-                            src="https://cdn.akamai.steamstatic.com/steam/apps/1940190/header.jpg?t=1649487691"
-                        /> 
-                    </Center>   
-                </SwiperSlide>
-            </Swiper>
+      <SwiperSlide>
+        <Image src="https://cdn.akamai.steamstatic.com/steam/apps/1021100/header.jpg?t=1650647590" />
+      </SwiperSlide>
 
-    )
-        
+      <SwiperSlide>
+        <Image src="https://cdn.akamai.steamstatic.com/steam/apps/1133760/header.jpg?t=1649772146" />
+      </SwiperSlide>
+    </Swiper>
+  );
 }
