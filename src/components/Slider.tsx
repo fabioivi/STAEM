95dcfb96cade1f314dcaff0678124caf0fe7359f
Fábio Duarte de Oliveira <fabio.ivi@gmail.com>
2022-05-03 23:03:13 -0400

    feat: add bullets of navigation in slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,18 +1,20 @@
-import { Box, Heading, Image, Center } from "@chakra-ui/react";
-import { useState } from "react";
+import { Image } from "@chakra-ui/react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+// import "swiper/css/scrollbar";
 
-import { EffectFade, Navigation, Pagination } from "swiper";
+import { Navigation, Pagination } from "swiper";
 
 export function Slider() {
   return (
     <Swiper
+      modules={[Navigation, Pagination]}
+      // navigation
+      pagination={{ clickable: true }}
       slidesPerView={2}
       spaceBetween={150}
       centeredSlides={true}
@@ -26,6 +28,14 @@ export function Slider() {
 
       <SwiperSlide>
         <Image src="https://cdn.akamai.steamstatic.com/steam/apps/1021100/header.jpg?t=1650647590" />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <Image src="https://cdn.akamai.steamstatic.com/steam/apps/1133760/header.jpg?t=1649772146" />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <Image src="https://cdn.akamai.steamstatic.com/steam/apps/1133760/header.jpg?t=1649772146" />
       </SwiperSlide>
 
       <SwiperSlide>
