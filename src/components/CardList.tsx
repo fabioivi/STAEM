5cacf7804f4ede8c8630d97a7270d5d5f0b668d9
Fabio Duarte <fabio.ivi@gmail.com>
2022-05-05 13:55:49 -0400

    fix: infinite scroll trigger twice

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,9 +1,5 @@
 import {
   Flex,
-  Text,
-  Heading,
-  Image as ImageUi,
-  Box,
   Spinner,
 } from "@chakra-ui/react";
 import { useEffect, useState, useRef } from "react";
@@ -40,8 +36,30 @@ export function CardList({ filter, setFilter }: TFilter) {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.25,
+    };
+
+    const observer = new IntersectionObserver((entities) => {
+      const target = entities[0];
+
+      if (target.isIntersecting) {
+        setPage((_page) => _page + 5);
+        console.log("carregando : " + page);
+      }
+    }, options);
+
+    if (loadMoreRef.current) {
+      observer.observe(loadMoreRef.current);
+    }
+  }, []);
+
+  useEffect(() => {
     const getGames = async () => {
       try {
+        setLoading(true);
         console.log("filter:" + filter.search);
         console.log("sort:" + filter.sort);
         console.log(page);
@@ -53,36 +71,16 @@ export function CardList({ filter, setFilter }: TFilter) {
           .order("title", { ascending: true })
           .limit(page);
         console.log(data);
+        setLoading(false);
         setGames(data);
+        
       } catch (error) {
+        setLoading(false);
         console.log(error);
       }
     };
     getGames();
-    setLoading(false);
   }, [filter.search, filter.sort, page]);
-
-  useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: "20px",
-      threshold: 1.0,
-    };
-
-    const observer = new IntersectionObserver((entities) => {
-      const target = entities[0];
-
-      if (target.isIntersecting && loading !== true) {
-        setLoading(true);
-        setPage((_page) => _page + 1);
-        console.log("carregando : " + page);
-      }
-    }, options);
-
-    if (loadMoreRef.current) {
-      observer.observe(loadMoreRef.current);
-    }
-  }, []);
 
   return (
     <Flex direction={"column"} bg="brand.darkblue" color="brand.white">
@@ -90,8 +88,8 @@ export function CardList({ filter, setFilter }: TFilter) {
       <Flex
         alignItems="center"
         justifyContent="center"
-        p="60px"
         ref={loadMoreRef}
+        h="300px"
       >
         {loading && (
           <Spinner
